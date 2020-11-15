48a58c2aae15ccb83a02a5494508d7d8391d09d3
vilmacio <vilmacio@users.noreply.github.com>
2020-11-15 11:35:57 -0300

    style: file div adjustment

--- view/script.js
+++ view/script.js
@@ -1,7 +1,7 @@
 async function getTree () {
   return new Promise(resolve => {
     // eslint-disable-next-line no-undef
-    fetch('../code/data.json')
+    fetch('../code/db/data.json')
       .then(response => response.json())
       .then(data => {
         resolve(data)
@@ -14,33 +14,16 @@ const sectionTree = document.getElementById('tree')
 async function createFile () {
   const data = await getTree()
 
-  for (const file of data) {
+  for (const file of data.tree) {
     const fileDiv = document.createElement('div')
-    fileDiv.setAttribute('class', 'file')
-
-    const gitStatusElement = document.createElement('p')
-    gitStatusElement.setAttribute('id', 'git-status')
-    const statusText = document.createTextNode(file.status[0])
-
-    const fileNameElement = document.createElement('p')
-    fileNameElement.setAttribute('id', 'file-name')
-    const fileNameText = document.createTextNode(file.name)
-
-    const labelElement = document.createElement('div')
-    labelElement.setAttribute('class', 'label')
-
-    const labelTextElement = document.createElement('p')
-    const labelText = document.createTextNode(file.path)
-
-    fileDiv.appendChild(gitStatusElement)
-    fileDiv.appendChild(fileNameElement)
-    fileDiv.appendChild(labelElement)
-
-    labelElement.appendChild(labelTextElement)
-    labelTextElement.appendChild(labelText)
-
-    gitStatusElement.appendChild(statusText)
-    fileNameElement.appendChild(fileNameText)
+    fileDiv.innerHTML = `
+    <div class="file">
+      <p id="git-status">${file.status[0]}</p>
+      <p id="file-name">${file.name}</p>
+      <div class="label">
+          <p>${file.path}</p>
+      </div>
+    </div>`
 
     sectionTree.appendChild(fileDiv)
   }
